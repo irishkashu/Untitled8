04ec901a2f913ad752a6def46c1c804806adce3a
admin <admin@admins-iMac-2.local>
2026-09-06 12:53:32 +0300

    commit

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Customer {
+import java.util.Objects;
+
+public class Customer {
+    private String id;
+    private String fullName;
+    private String email;
+
+    public Customer(String id, String fullName, String email){
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+    public boolean equals(Object object){
+        return hashCode() == object.hashCode();
+    }
+    public int hashCode(){
+        return Objects.hash(email);
+    }
 }
