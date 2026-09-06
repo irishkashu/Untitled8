7270ccfbc4b82d4ba28d039b6f551b7f33c9d50a
admin <admin@admins-iMac-2.local>
2026-09-06 13:00:02 +0300

    commit

--- src/Order.java
+++ src/Order.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Order {
+import java.util.HashMap;
+
+public class Order {
+    private HashMap<Product, Integer> products;
+    private String orderId;
+    private String status;
+
+    public Order(String orderId, String status){
+        this.orderId = orderId;
+        this.status = status;
+    }
+    public void addProduct(Product product, int quantity){
+        if()
+    }
 }
