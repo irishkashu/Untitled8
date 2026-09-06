86d38f3e4ed0a0c1fdb624cf24a7ceecf313ea64
admin <admin@admins-iMac-2.local>
2026-09-06 12:32:24 +0300

    init commit

---
+++ src/Product.java
@@ -0,0 +1,31 @@
+import java.util.Objects;
+
+public class Product {
+    private String name;
+    private double price;
+    private String category;
+
+    public Product(String name, double price, String category){
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+    public boolean equals(Object object){
+        return object.hashCode() == this.hashCode();
+    }
+    public int hashCode(){
+        return Objects.hash(name);
+    }
+}
